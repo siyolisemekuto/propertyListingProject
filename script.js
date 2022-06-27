4ed786a115260e0aa35065c47df7f33c9f533e9c
siyolisemekuto <103102826+siyolisemekuto@users.noreply.github.com>
2022-06-27 15:25:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+//navbar
+function toggleNav(){ document.querySelector("#navbar").classList.toggle("active")
+}
+
+
+let properties = [
+    {
+        id:1,
+        image:"",
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$876 000",
+        specifications:{
+            bedroooms: 3,
+            bathrooms: 2,
+            garage: 2,
+            size:"2000 Sq ft"
+        },
+    },
+    {
+        id:2,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$770 000",
+        specifications:{
+            bedroooms: 2,
+            bathrooms: 2,
+            garage: 1,
+            size:"1800 Sq ft"
+        },
+    },
+    {
+        id:3,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$560 000",
+        specifications:{
+            bedroooms: 1,
+            bathrooms: 1,
+            size:"1540 Sq ft"
+        },
+    },
+    {
+        id:4,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$600 000",
+        specifications:{
+            bedroooms: 2,
+            bathrooms: 2,
+            garage: 1,
+            size:"1700 Sq ft"
+        },
+    },
+    {
+        id:5,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$680 000",
+        specifications:{
+            bedroooms: 2,
+            bathrooms: 1,
+            garage: 1,
+            size:"1700 Sq ft"
+        },
+    },
+    {
+        id:6,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$700 000",
+        specifications:{
+            bedroooms: 2,
+            bathrooms: 2,
+            garage: 2,
+            size:"1800 Sq ft"
+        },
+    },
+    {
+        id:7,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$580 000",
+        specifications:{
+            bedroooms: 2,
+            bathrooms: 1,
+            garage: 1,
+            size:"1500 Sq ft"
+        },
+    }, 
+    {
+        id:8,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$830 000",
+        specifications:{
+            bedroooms: 3,
+            bathrooms: 2,
+            garage: 2,
+            size:"2200 Sq ft"
+        },
+    },
+    {
+        id:9,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$790 000",
+        specifications:{
+            bedroooms: 3,
+            bathrooms: 2,
+            garage: 1,
+            size:"2500 Sq ft"
+        },
+    },
+    {
+        id:10,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$785 000",
+        specifications:{
+            bedroooms: 2,
+            bathrooms: 1,
+            garage: 1,
+            size:"1950 Sq ft"
+        },
+    },
+    {
+        id:11,
+        title: "Apartment",
+        location: "Cape Town",
+        price: "$670 000",
+        specifications:{
+            bedroooms: 1,
+            bathrooms: 1,
+            garage: 1,
+            size:"1700 Sq ft"
+        },
+    }
+]
